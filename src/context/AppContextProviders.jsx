74a490bab3ba9d810a43e5f8be6d9ae1f793a3ef
sohan-me-01 <sohanshak850@gmail.com>
2025-08-15 00:35:43 +0600

    course details in student compoents work is done

--- src/context/AppContextProviders.jsx
+++ src/context/AppContextProviders.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { AppContext } from "./AppContext"
 import { dummyCourses } from "../assets/asset/assets";
 import { useNavigate } from "react-router";
+import humanizeDuration from "humanize-duration";
 
 
 export const AppContextProvider = ({ children }) => {
@@ -30,13 +31,39 @@ export const AppContextProvider = ({ children }) => {
         })
         return totalRating / course.courseRatings.length
     }
+
+    // Function to Calculate Course Chapter Time.
+    const calculateChapterTime = (chapter) => {
+        let time = 0;
+        chapter.chapterContent.map((lecture)=> time += lecture.lectureDuration);
+        return humanizeDuration(time * 60 * 1000, {units : ["h", "m"]})
+    }
+    // Function to Calculate Course Duration
+    const calculateCourseDuration = (course) => {
+        let time = 0;
+        course.courseContent.map((chapter) => chapter.chapterContent.map((lecture) => time += lecture.lectureDuration))
+        return humanizeDuration(time * 60 * 1000, {units : ["h", "m"]})
+    }
+    // Function to Calculate to number of lectures in the course.
+    const calculateNumberOfLectures = (course) => {
+        let totalLectures = 0;
+        course.courseContent.forEach(chapter => {
+            if(Array.isArray(chapter.chapterContent)) {
+                totalLectures += chapter.chapterContent.length;
+            }
+        });
+        return totalLectures; 
+    }
     const userInfo = {
         currency,
         allCourses,
         navigate,
         calculateRating,
         isEducator,
-        setIsEducator
+        setIsEducator,
+        calculateChapterTime,
+        calculateCourseDuration,
+        calculateNumberOfLectures
     }
     return (
         <AppContext.Provider value={userInfo}>
